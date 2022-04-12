e0166cc2075573ec8c04128c4d189b91645aff64
Maksim Zlivko <maksmoney87@gmail.com>
2022-04-12 21:21:27 +0500

    fix

--- web/webhook.php
+++ web/webhook.php
@@ -3,11 +3,15 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
+$config = require __DIR__ . '/../config/web.php';
+
+$yii = (new yii\web\Application($config));
+
 try {
     $data = file_get_contents("php://input");
+    Yii::error(gettype($data), 'tmf_helper');
 } catch (Exception $e) {
-    \Yii::error($e->getMessage(), 'tmf_helper');
+    Yii::error($e->getMessage(), 'tmf_helper');
 }
-\Yii::error('test2', 'tmf_helper');
 
 return http_response_code(200);
